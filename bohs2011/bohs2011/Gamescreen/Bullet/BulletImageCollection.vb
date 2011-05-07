09c55c7e4cd5d9fb1aeb369d95acb5fd3e5d02ff
Michael Pollind <devnull@localhost>
2011-05-06 20:28:32 -0700

    added missing files

---
+++ bohs2011/bohs2011/Gamescreen/Bullet/BulletImageCollection.vb
@@ -0,0 +1,3 @@
+﻿Public Class BulletImageCollection
+    Public Shared Bullet1 As Bitmap = New Bitmap("Gamescreen\Bullet\Bullets\FastFire\Assets\monster.png")
+End Class
---
+++ bohs2011/bohs2011/Gamescreen/Bullet/Bullets/FastFire/FastfireBullet.vb
@@ -0,0 +1,17 @@
+﻿Public Class FastfireBullet
+    Inherits Bullets
+    Public Sub New()
+        Me.killEntities = True
+        Me.size = New Point(10, 10)
+    End Sub
+    Public Overrides Sub Paint(ByVal e As System.Windows.Forms.PaintEventArgs)
+        e.Graphics.DrawImage(BulletImageCollection.Bullet1, location)
+        e.Graphics.DrawRectangle(Pens.Black, New Rectangle(Me.location, Me.size))
+        MyBase.Paint(e)
+    End Sub
+
+    Public Overrides Sub Update()
+        Me.location += Me.direction
+        MyBase.Update()
+    End Sub
+End Class
